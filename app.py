e3195b44024aee9968970845c64c9e302ad0fc01
Sharon CR <schullunquia@unsa.edu.pe>
2022-06-29 22:31:47 -0500

    :sparkles: Added sankey diagram

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
 import json
 # render_template: para retornar archivos Ex. index.html
 # request: para recibir data dada por el usuario
@@ -9,9 +10,14 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
-@app.route("/get-data")
-def get_data():
+@app.route("/get-stations")
+def get_stations():
   with open("static/data/stations.json") as f:
+    return json.load(f)
+
+@app.route("/get-trips")
+def get_data():
+  with open("static/data/viajes.json") as f:
     return json.load(f)
 
 if __name__ == "__main__":
